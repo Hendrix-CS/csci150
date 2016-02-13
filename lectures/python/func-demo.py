cf9449ce36b19f377ab2b0f95d7c2057ab9475a4
Brent Yorgey <byorgey@gmail.com>
2016-02-13 14:01:26 -0600

    func-demo from in class

--- lectures/python/func-demo.py
+++ lectures/python/func-demo.py
@@ -54,10 +54,75 @@ def better_C_to_F(temp_C):
 
     return F     # stop and immediately give F as output
 
-
-
-
-
-
-
+# Book calls these "fruitful functions" (Chapter 5)
     
+def better_C_to_F_2(temp_C):
+    F = 1.8*temp_C + 32
+    # F has the same name as F from C_to_F but they
+    #   are completely unrelated.
+
+    return F     # stop and immediately give F as output
+    print "Done converting!"   # this never happens!
+
+def better_C_to_F_3(temp_C):
+    return 1.8*temp_C + 32
+    # this does the same thing as better_C_to_F
+
+def F_to_qualitative(temp_F):
+    if temp_F < 32:
+        return "freezing"
+    elif temp_F < 60:
+        return "cold"
+    elif temp_F < 80:
+        return "warm"
+    elif temp_F < 100:
+        return "hot"
+    else:
+        return "ouchies"
+
+# Prompt the user for temp in F
+# Make a comment like "it is warm today".
+def discuss_weather_in_US():
+    temp_F = int(raw_input("What is the temperature in degrees F? "))
+    word = F_to_qualitative(temp_F)
+    print "Hmm, it is very " + word + " today, don't you think?"
+
+
+# Prompt the user for temp in C
+# Make a comment like "it is warm today".
+def discuss_weather_in_metric():
+    temp_C = int(raw_input("What is the temperature in degrees C? "))
+    temp_F = better_C_to_F_2(temp_C)
+    word = F_to_qualitative(temp_F)
+    print "Hmm, it is very " + word + " today, don't you think?"
+
+# Prompt the user for temp in C
+# Make a comment like "it is warm today".
+def discuss_weather_in_metric_2():
+    temp_C = int(raw_input("What is the temperature in degrees C? "))
+    word = F_to_qualitative(better_C_to_F_2(temp_C))
+    print "Hmm, it is very " + word + " today, don't you think?"
+
+# Prompt the user for temp in C
+# Make a comment like "it is warm today".
+def discuss_weather_in_metric_3():
+    temp_C = int(raw_input("What is the temperature in degrees C? "))
+    print "Hmm, it is very " + F_to_qualitative(better_C_to_F_2(temp_C)) + " today, don't you think?"
+
+
+# Prompt the user for temp in C
+# Make a comment like "it is warm today".
+# DO NOT TRY THIS AT HOME
+def discuss_weather_in_metric_4():
+    print "Hmm, it is very " + F_to_qualitative(better_C_to_F_2(int(raw_input("What is the temperature in degrees C? ")))) \
+          + " today, don't you think?"
+
+# A function with two inputs
+def is_divisible(x, y):
+    if (x % y == 0):
+        return True
+    else:
+        return False
+
+def better_is_divisible(x, y):
+    return (x % y == 0)
