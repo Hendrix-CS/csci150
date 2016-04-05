2422e25f8d76f1e4c727a7e17a0931e05e0c523b
Brent Yorgey <byorgey@gmail.com>
2016-04-05 06:38:23 -0500

    work on Lab 10 (debugging)

--- labs/lab10-debugging/sum-practice-test.py
+++ labs/lab10-debugging/sum-practice-test.py
@@ -1,14 +1,14 @@
 # Calculate the sum of the first N odd numbers.
-def Main():
+def main():
     print "We will compute the sum of some odd numbers."
     n = raw_input("How many odd numbers would you like to add? ")
     count = 0
     odd = 1
-total = 0
+    total = 0
 
     while (count != n):
         total = total + odd
-        odd = Odd + 1
+        odd = odd + 1
         count += 1
 
     print "The sum of the first " + str(n) + " odd numbers is " + str(total) + "."
