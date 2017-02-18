f5e8368a2501e354fb45fdec4cb6bc91a3292598
Brent Yorgey <byorgey@gmail.com>
2017-02-17 19:21:17 -0600

    code from lecture

--- web/static/collatz.py
+++ web/static/collatz.py
@@ -17,3 +17,22 @@ def collatz(n):
         count = count + 1
         n = hailstone(n)
     return count
+
+# Input: max (int)
+# Output: none
+#
+# Test numbers from 1 up to max and print # of iterations
+#  required for each.
+def test_collatz(max):
+    n = 1
+    max_iterations = 0
+    max_n = 0
+    while True:
+        iters = collatz(n)
+        if max_iterations < iters:
+            max_iterations = iters
+            max_n = n
+            
+            print(str(max_n) + " required " + str(max_iterations) + " iterations.")
+
+        n += 1
