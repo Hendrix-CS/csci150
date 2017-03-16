20be1c5da96c6aa87fbb3db009068462e9f027a8
Brent Yorgey <byorgey@gmail.com>
2017-03-15 21:43:49 -0500

    more recursion class notes

--- lectures/S17/recursion.py
+++ lectures/S17/recursion.py
@@ -41,8 +41,37 @@ def listsum(nums):
     else:
         return nums[0] + listsum(nums[1:])
 
-
-
-
+# Input: list of ints
+# Output: product of all the numbers in the list (int).
+def listprod(nums):
+    if nums == []:
         
 
+
+####################################
+
+# Thought process to write factorial_rec function!
+
+# 1. Write inputs/outputs!
+
+# Input: n (int)
+# Output: an int which is the factorial of n (1 * 2 * 3 * ... * n)
+#   Note that 0! = 1.
+def fact(n):
+    # 2. Think of simple cases for the input ("base cases") where
+    #   we can return the answer immediately.
+    if n == 0:
+        return 1
+##    elif n == 1:   # this base case is unnecessary
+##        return 1
+
+    else:
+        # 3. What if I knew the answer to a simpler problem?
+        # e.g. fact(n-1) ?
+        #
+        # "Leap of faith": ASSUME that fact(n-1) will work correctly.
+        # If it does, how can we use its answer to compute the
+        # correct output to fact(n) ?
+        #
+        # In this case: we need to multiply fact(n-1) by n.
+        return n * fact(n-1)
