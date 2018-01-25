372b92b3770809a19ca4642952e98e592f850d88
Brent Yorgey <byorgey@gmail.com>
2018-01-24 22:55:23 -0600

    in-class code

---
+++ web/static/VariablesIO.py
@@ -0,0 +1,13 @@
+print("Hello, CSCI 150!")
+print("Here is another print")
+
+x = 4
+print(x + 5)
+
+name = input("What is your name? ")
+print("Welcome, " + name + "!")
+
+z = 99
+
+age = int(input("What is your age in seconds? "))
+print(3.2 + age)
