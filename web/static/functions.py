971ccc088d8f8110c05208d9d3d70215ed3f280e
Brent Yorgey <byorgey@gmail.com>
2018-02-14 13:53:04 -0600

    in-class code on functions

--- web/static/functions.py
+++ web/static/functions.py
@@ -1,36 +1,29 @@
-# Making our own functions
-
-# def = keyword to define a function
-# say_hi is the name of the function
-# () means it needs no inputs.
-
-# This is a recipe for how to do something.
-def say_hi():
-    print("Hello there, CSCI 150!")
-    print("This is a function.")
-
-say_hi()    # You ALWAYS NEED PARENTHESES.
+# Defining our own functions!
 
 def say_hi_twice():
     say_hi()
     say_hi()
 
-# temp_C is the name of the input to C_to_F.
-# It is called a "parameter".
+# Keyword 'def' for defining a function
+def say_hi():
+    print("Hello, CSCI 150!")
+    print("This is a function")
 
-# temp_C is a float --- the temperature in Celsius
-def C_to_F_bad(temp_C):
-    # temp_F is a local variable -- only exists inside the function.
-    temp_F = temp_C * 9/5 + 32
-    print("The temperature in degrees Fahrenheit in " + str(temp_F))
+# Function with an input
+# temp_C is a *parameter*
+def C_to_F_msg(temp_C: int):
+    F = temp_C * 9 / 5 + 32
+    print("The temperature in degrees F is " + str(F) + "!!!")
 
-C_to_F_bad(37)   # 37 is an "argument".
+# To use C_to_F_msg: e.g.   C_to_F_msg(15)
+# 15 is an *argument*
 
-# C_to_F is not as useful as it could be because it can only
-# print a specific message, and you can't recover the converted
-# value later.
+# F (or any variable defined inside a function) is *local* --- only
+# accessible ("in scope") inside the body of the function.
+# Parameters are also local.
 
-def C_to_F(temp_C):
-    temp_F = temp_C * 9/5 + 32
-    return temp_F
+# C_to_F_msg should actually be decomposed!  It is doing too much at once.
 
+def C_to_F(temp_C: float) -> float:
+    F = temp_C * 9 / 5 + 32
+    return F
--- web/syllabus.md
+++ web/syllabus.md
@@ -24,10 +24,8 @@ to review the material and come to class prepared.
 * Wed, Feb 7: Exam I review
 * Fri, Feb 9: Exam I (**Chapters 1-4**)
 * Mon, Feb 12: Repetition I (while loops) [ [while.py](static/while.py) ] (**Chapter 7**)
+* Wed, Feb 14: Functions I [ [functions.py](static/functions.py) ] (**Chapters 5 and 6**)
 
-<!-- * Chapter 5: Fri, February 10 [ [functions.py](static/functions.py) [functions.txt](static/functions.txt) ] -->
-<!-- * Chapter 6: Mon, February 13 [ [functions2.py](static/functions2.py) [functions2.txt](static/functions2.txt) ] -->
-<!-- * Chapter 7: Weds, February 15 [ [while.py](static/while.py) [collatz.py](static/collatz.py) [while-collatz.txt](static/while-collatz.txt) ] -->
 <!-- * Chapter 8: Fri, February 17 [ [strings.py](static/strings.py) [strings.txt](static/strings.txt) ] -->
 <!-- * Wed, Feb 22 [ [box-functions.py](static/box-functions.py) [box-functions.txt](static/box-functions.txt) ] -->
 <!-- * Chapter 9: Fri, February 24 [ [lists.py](static/lists.py) [lists.txt](static/lists.txt) ] -->
