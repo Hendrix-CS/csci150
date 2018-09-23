d49a2797d489445b185e2096333658f15742edf1
Brent Yorgey <byorgey@gmail.com>
2018-09-22 22:12:12 -0500

    update syllabus and class notes

--- web/static/functions.py
+++ web/static/functions.py
@@ -19,6 +19,16 @@ def say_hi():   # inputs go in the parentheses
     print("This is a function.")
     input("What is your name? ")
     print("Whatever.")
+    print(5/0)
+
+def say_hi_a_lot():
+    say_hi_twice()
+    say_hi()
+    say_hi_twice()
+
+def even_more_hi():
+    say_hi_a_lot()
+    say_hi_a_lot()
 
 # say_hi_twice()
 
@@ -33,6 +43,7 @@ def C_to_F_message(temp_C: float):
 def C_to_F_interactive():
     temp_C: float = float(input("What is the temperature in degrees C? "))
     C_to_F_message(temp_C)
+    print("Have a nice day!")
 
 # temp_F is a *local variable*: only defined inside the function.
 
@@ -44,3 +55,35 @@ def C_to_F(temp_C: float) -> float:
 
     # Alternatively:
     return temp_C * 9/5 + 32
+
+# Convert a temperature in degrees F to a descriptive adjective.
+def F_to_qualitative(temp_F: float) -> str:
+    if temp_F < 0:
+        return "frigid"
+    elif temp_F <= 32:
+        return "freezing"
+    elif temp_F <= 50:
+        return "cold"
+    elif temp_F <= 75:
+        return "nice"
+    elif temp_F <= 90:
+        return "warm"
+    elif temp_F <= 110:
+        return "hot"
+    elif temp_F <= 150:
+        return "blazing"
+    else:
+        return "dead"
+
+# Prompt the user for the temperature in degrees C and make an appropriate comment
+def discuss_weather():
+    temp_C: float = float(input("What is the temperature in degrees C? "))
+    temp_F: float = C_to_F(temp_C)
+    word: str = F_to_qualitative(temp_F)
+    print("My, it's very " + word + " today, don't you think?")
+
+discuss_weather()
+
+# Prompt the user for the temperature in degrees C and make an appropriate comment
+def discuss_weather2():
+    print("My, it's very " + F_to_qualitative(C_to_F(float(input("What is the temperature in degrees C? ")))) + " today, don't you think?")
