36b761f2495252bd3fc9803cc2ab538c124eea50
Brent Yorgey <byorgey@gmail.com>
2019-09-23 09:31:29 -0500

    upload in-class code

---
+++ in-class/8am/intro.py
@@ -0,0 +1,16 @@
+x = 3 + 4
+
+print('Hello!')
+print(x)
+print( f'The value of x is: {x}' )
+print( f'Two more than x is: {x+2}')
+
+# Print out the prompt, wait for the user to type something,
+# return what they typed as a str.
+name = input('What is your name? ')
+print(f'Welcome, {name}!')
+
+fav_num_str = input('What is your favorite number? ')
+fav_num = int(fav_num_str)
+print(f'Two more than your favorite number is {fav_num + 2}.')
+
---
+++ in-class/8am/while.py
@@ -0,0 +1,17 @@
+
+# Repetition in Python!
+
+# while <condition>:
+#   stuff1
+#   stuff2
+#   ...
+
+# if executes 0 or 1 time
+# while executes any number of times (zero or more)
+
+# Example
+
+count = 0
+while count < 10:
+    print(count)
+    count = count + 1
