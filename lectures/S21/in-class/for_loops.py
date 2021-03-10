f6f9bb56f444a3cf2f504bcc5d2627505d8b0853
Brent Yorgey <byorgey@gmail.com>
2021-03-10 09:27:28 -0600

    in-class code

--- lectures/S21/in-class/for_loops.py
+++ lectures/S21/in-class/for_loops.py
@@ -79,3 +79,75 @@ def contains3(haystack: List[int], needle: int) -> bool:
         found = found or (n == needle)
 
     return found
+
+# Return the index of the first occurrence of the character
+# 'needle' in the string 'haystack', or -1 if it is not
+# found.
+#
+#   e.g. find('banana', 'a') = 1
+#        find('banana', 'n') = 2
+#        find('banana', 'z') = -1
+# def find(haystack: str, needle: str) -> int:
+#     for c in haystack:
+#         if c == needle:
+#             # We're stuck!  We need to return the *index*
+#             # of c, but we don't have that information.
+#             return ???
+#
+#     # Didn't find it
+#     return -1
+
+# Reasons we might want to use  for i in range(...):
+#   1. We want to use or return the index
+#   2. We want to modify the elements of the list
+#   3. We want to look at more than one element of the list
+#      at a time.
+
+def find(haystack: str, needle: str) -> int:
+    # conceptually we want this:
+    # for i in [0, 1, 2, 3, ..., len(haystack) - 1]:
+
+    # the range function can make lists like this for us.
+    # This will loop over the list of all valid indices
+    # for the string haystack.
+    for i in range(len(haystack)):
+        if haystack[i] == needle:
+            return i
+
+    return -1
+
+animals: List[str] = ['capybara', 'squid', 'orca', 'lemur']
+
+for animal in animals:
+    animal += '!'
+
+# >>> animals
+# ['capybara', 'squid', 'orca', 'lemur']
+# >>> x = animals[2]
+# >>> x
+# 'orca'
+# >>> x += '!'
+# >>> x
+# 'orca!'
+# >>> animals
+# ['capybara', 'squid', 'orca', 'lemur']
+# >>> animals[2] += '!'
+# >>> animals
+# ['capybara', 'squid', 'orca!', 'lemur']
+
+# If we actually want to change the entries in animals:
+
+for i in range(len(animals)):
+    animals[i] += '!'
+
+# Return whether the list is in order from smallest
+# to biggest.
+#
+#   e.g. is_sorted([2,5,6,9,9]) = True
+#        is_sorted([2,5,3,9,9]) = False
+#        is_sorted([]) = True
+def is_sorted(nums: List[int]) -> bool:
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i+1]:
+            return False
+    return True
