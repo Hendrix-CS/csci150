6db398642ae5475537fa49bda70b4b1e4bf2bab5
Brent Yorgey <byorgey@gmail.com>
2021-03-17 09:13:46 -0500

    check in in-class code

--- lectures/S21/in-class/dictionaries.py
+++ lectures/S21/in-class/dictionaries.py
@@ -80,6 +80,44 @@ def frequency_counts2(s: str) -> Dict[str, int]:
     for c in s:
         if c not in counts:
             counts[c] = 0
-
         counts[c] += 1
     return counts
+
+# Normalize a dictionary of counts by dividing each count
+# by the total.
+#   e.g. normalize({'a': 2, 'b': 5, 'c': 3})
+#      = {'a': 0.2, 'b': 0.5, 'c': 0.3}
+def normalize(counts: Dict[str,int]) -> Dict[str,float]:
+
+    # First, compute the total count by adding up
+    # all the individual counts
+    total: int = 0
+    for key in counts:
+        total += counts[key]
+
+    # Now, compute the frequency for each character
+    # by dividing the count of that character
+    # by the total count.
+    freqs: Dict[str, float] = {}
+    for key in counts:
+        freqs[key] = counts[key] / total
+
+    return freqs
+
+# Given a file name, compute the frequencies of all
+# characters that occur in the file.
+#
+# e.g. file_freqs('dictionaries.py') = {'a': ....}
+def file_freqs(file_name: str) -> Dict[str, float]:
+    file = open(file_name, 'r')
+    contents: str = file.read()
+    file.close()
+    return normalize(frequency_counts(contents))
+
+def file_word_freqs(file_name: str) -> Dict[str, float]:
+    file = open(file_name, 'r')
+    words: List[str] = []
+    for line in file.readlines():
+        words += line.strip().split()
+    file.close()
+    return normalize(frequency_counts(words))
