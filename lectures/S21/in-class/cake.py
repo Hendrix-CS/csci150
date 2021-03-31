9faf3aea302a4792ba6dcb068ae82fe3176369f6
Brent Yorgey <byorgey@gmail.com>
2021-03-31 13:15:50 -0500

    post in-class code

--- lectures/S21/in-class/cake.py
+++ lectures/S21/in-class/cake.py
@@ -25,15 +25,41 @@ class Cake:    # names of classes traditionally start
     # currently lit
     num_candles: int
 
+    # Number of times we have blown out some candles
+    num_blows: int
+
     # Pick a random number of candles and blow them out.
     def blowout(self):
         r = random.randint(1, self.num_candles)
         self.num_candles -= r
+        self.num_blows += 1
 
+    # Ask whether all the candles are out.
+    def all_out(self) -> bool:
+        return self.num_candles == 0
+
+    def get_num_blows(self) -> int:
+        return self.num_blows
+
+# Keep blowing out candles on the cake c until all out,
+# and return the number of blows required.
+def happy_birthday(c: Cake) -> int:
+    while not c.all_out():
+        c.blowout()
+    return c.get_num_blows()
+
+
+def average_blows(num_candles: int, num_cakes: int) -> float:
+    total: int = 0
+    for i in range(num_cakes):
+        c: Cake = Cake(num_candles, 0)
+        total += happy_birthday(c)
+
+    return total / num_cakes
 
 def main():
-    my_cake: Cake = Cake(100)
-    while my_cake.num_candles > 0:
-        my_cake.blowout()
+    for num_candles in range(500):
+        avg: float = average_blows(num_candles, 1000)
+        print(f'{num_candles} {avg}')
 
 main()
