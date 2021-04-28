80bd8800005e693776e3599d09df7aeb851d0dbf
Brent Yorgey <byorgey@gmail.com>
2021-04-28 09:17:16 -0500

    in-class paint code

--- lectures/S21/in-class/paint.py
+++ lectures/S21/in-class/paint.py
@@ -17,21 +17,47 @@ class Stroke:
         if len(self.points) >= 2:
             pygame.draw.lines(surface, self.color, False, self.points, self.stroke_width)
 
+    def inside(self, rect: Rect) -> bool:
+        for p in self.points:
+            if not rect.contains(p):
+                return False
+        return True
+
+    def move(self, v: Tuple[int, int]):
+        for i in range(len(self.points)):
+            self.points[i] = \
+                (self.points[i][0] + v[0], self.points[i][1] + v[1])
+
+    def copy(self):
+        return Stroke(self.points.copy(), self.color, self.stroke_width)
+
 @dataclass
 class Rect:
-    upper_left: Tuple[int,int]
-    lower_right: Tuple[int,int]
+    ul: Tuple[int, int]
+    lr: Tuple[int, int]
     color: str
     stroke_width: int
 
     def update(self, p: Tuple[int,int]):
-        self.lower_right = p
+        self.lr = p
 
     def draw(self, surface):
-        w: int = self.lower_right[0] - self.upper_left[0]
-        h: int = self.lower_right[1] - self.upper_left[1]
-        pygame.draw.rect(surface, self.color, (self.upper_left, (w,h)), self.stroke_width)
+        w: int = self.lr[0] - self.ul[0]
+        h: int = self.lr[1] - self.ul[1]
+        pygame.draw.rect(surface, self.color, (self.ul, (w, h)), self.stroke_width)
 
+    def contains(self, p: Tuple[int, int]) -> bool:
+        return self.ul[0] <= p[0] <= self.lr[0] and self.ul[1] <= p[1] <= self.lr[1]
+
+    def inside(self, rect: 'Rect') -> bool:
+        return rect.contains(self.ul) and rect.contains(self.lr)
+
+    def move(self, v: Tuple[int, int]):
+        self.ul = (self.ul[0] + v[0], self.ul[1] + v[1])
+        self.lr = (self.lr[0] + v[0], self.lr[1] + v[1])
+
+    def copy(self):
+        return Rect(self.ul, self.lr, self.color, self.stroke_width)
 
 @dataclass
 class Line:
@@ -46,27 +72,80 @@ class Line:
     def draw(self, surface):
         pygame.draw.line(surface,self.color,self.start,self.end,self.stroke_width)
 
+    def inside(self, rect: Rect) -> bool:
+        return rect.contains(self.start) and rect.contains(self.end)
+
+    def move(self, v: Tuple[int, int]):
+        self.start = (self.start[0] + v[0], self.start[1] + v[1])
+        self.end = (self.end[0] + v[0], self.end[1] + v[1])
+
+    def copy(self):
+        return Line(self.start, self.end, self.color, self.stroke_width)
+
+@dataclass
+class Group:
+    things: list
+
+    def append(self, thing):
+        self.things.append(thing)
+
+    def pop(self):
+        return self.things.pop()
+
+    def draw(self, surface):
+        for thing in self.things:
+            thing.draw(surface)
+
+    def __len__(self) -> int:
+        return len(self.things)
+
+    def select(self, rect: Rect) -> 'Group':
+        g = Group([])
+        for thing in self.things:
+            if thing.inside(rect):
+                g.append(thing.copy())
+        return g
+
+    def move(self, v: Tuple[int,int]):
+        for thing in self.things:
+            thing.move(v)
+
+@dataclass
+class Moving:
+    thing: Any
+    old_point: Tuple[int, int]
+
+    def update(self, new_point: Tuple[int, int]):
+        v = (new_point[0] - self.old_point[0], new_point[1] - self.old_point[1])
+        self.thing.move(v)
+        self.old_point = new_point
+
+    def draw(self, surface):
+        self.thing.draw(surface)
+
+
 # PaintState keeps track of the current state of our
 # paint program
 @dataclass
 class PaintState:
     surface: pygame.Surface
-    things: list
-    undone_things: list
+    things: Group
+    undone_things: Group
+    selected: Group = None
     cur_thing = None
     stroke_width: int = 1
 
     select_mode: bool = False
+    paste_mode: bool = False
 
     cur_tool: str = 'pencil'
     cur_color: str = 'black'
 
     def draw(self):
         self.surface.fill('white')
-        for stroke in self.things:
-            stroke.draw(self.surface)
+        self.things.draw(self.surface)
 
-        if self.select_mode:
+        if self.select_mode or self.paste_mode:
             self.cur_thing.draw(self.surface)
 
         pygame.display.update()
@@ -84,13 +163,17 @@ class PaintState:
         elif self.cur_tool == 'select':
             self.cur_thing = Rect(p, p, 'black', 1)
             self.select_mode = True
+        elif self.cur_tool == 'paste':
+            if self.selected is not None:
+                self.cur_thing = Moving(self.selected, p)
+            self.paste_mode = True
 
         # Set the initial (x,y) coordinates
         self.cur_thing.update(p)
 
         # Add the new current thing to the list of
         # all things.
-        if self.cur_tool != 'select':
+        if self.cur_tool != 'select' and self.cur_tool != 'paste':
             self.things.append(self.cur_thing)
 
     # Update the position of the mouse while dragging
@@ -98,7 +181,13 @@ class PaintState:
         self.cur_thing.update(p)
 
     def finish(self):
-        self.select_mode = False
+        if self.select_mode:
+            self.selected = self.things.select(self.cur_thing)
+            self.select_mode = False
+        elif self.paste_mode:
+            for thing in self.cur_thing.thing.things:
+                self.things.append(thing)
+            self.paste_mode = False
 
     def set_stroke_width(self, w: int):
         self.stroke_width = w
@@ -111,19 +200,19 @@ class PaintState:
 
     def undo(self):
         if len(self.things) > 0:
-            thing = self.things.pop(-1)
+            thing = self.things.pop()
             self.undone_things.append(thing)
 
     def redo(self):
         if len(self.undone_things) > 0:
-            thing = self.undone_things.pop(-1)
+            thing = self.undone_things.pop()
             self.things.append(thing)
 
 def main():
     pygame.init()
 
     surface = pygame.display.set_mode((640, 400))
-    state: PaintState = PaintState(surface, [], [])
+    state: PaintState = PaintState(surface, Group([]), Group([]))
 
     running = True
 
@@ -183,7 +272,8 @@ tool_dict: Dict[str, str] = {
     'p': 'pencil',
     'r': 'rect',
     'l': 'line',
-    's': 'select'
+    's': 'select',
+    'v': 'paste'
 }
 
 if __name__ == '__main__':
