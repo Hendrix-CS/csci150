adaf67f91cdea675b7e9e9afdb0ed5bb15db1c03
Brent Yorgey <byorgey@gmail.com>
2022-10-05 12:12:48 -0500

    Yorgey 10/5

--- code/fall2022/lists.py
+++ code/fall2022/lists.py
@@ -50,10 +50,92 @@ def sum_list(nums: List[int]) -> int:
 # return a list of all the inputs (excluding 'quit').
 def read_inputs() -> List[str]:
     user_input = input("Enter a string: ")
-    list = []
+    list: List[str] = []
     while user_input != "quit":
         list.append(user_input)
         user_input = input("Enter a string: ")
     return list
 
-print(read_inputs())
+# print(read_inputs())
+
+# More stuff with lists
+
+# We can use the 'in' operator to test whether something is an element of
+# a list or not.
+
+print(3 in [1,2,3,4,5])
+print(7 in [1,2,3,4,5])
+print('n' in 'banana')
+
+# Strings and lists
+
+# Convert a string to a list of characters using list(...)
+
+print("hello")
+print(list("hello"))
+
+# We can use .split(separator) to split a string into chunks
+
+animal_str = "dog,cat,horse,fly,prairie dog"
+print(animal_str.split(','))
+
+sentence = "Hi     there, CSCI   150!"
+print(sentence.split(' '))
+print(sentence.split())     # splits on multiple spaces
+
+# Turning a list into a string --- using 'join'
+# separator.join(list)   --- glues all the strings in 'list' into one big string
+#    with 'separator' in between each.
+
+steps = ['dog', 'cog', 'cot', 'cat']
+greeting = ["hi", "there", "CSCI", "150"]
+print('->'.join(steps))
+print(' '.join(greeting))
+print(''.join(greeting))
+print('\n'.join(greeting))
+
+# More example functions with lists
+
+# Find the product of the list of integers.
+#   e.g.  prod_list([2,5,3]) == 30
+def prod_list(nums: List[int]) -> int:
+    count: int = 0     # keep track of current index in list
+    prod: int = 1       # keep track of current prod
+    while count < len(nums):
+        prod *= nums[count]
+        count += 1
+    return prod
+
+print(prod_list([2,5,3]))
+print(prod_list([]))
+
+# Count how many integers in 'lst' are smaller than 'n'.
+#   e.g.  small_count([1,5,8,2,88,3,6], 7) == 5    (5 elements in the list are < 7).
+def small_count(lst: List[int], n: int) -> int:
+    i: int = 0          # keep track of current index in list
+    count: int = 0       # keep track of current count
+    while i < len(lst):
+        if lst[i] < n:
+            count += 1
+        i += 1
+    return count
+
+
+print(small_count([1,5,8,2,88,3,6], 7))
+print(small_count([1,5,8,2,88,3,6], 0))
+
+# Given a list of strings which are all lowercase letters, test whether
+# the list is in alphabetical order.
+#
+#   e.g.   alpha_order(['cat', 'dog', 'horse', 'platypus']) == True
+#   e.g.   alpha_order(['cat', 'dog', 'zebra', 'horse', 'platypus']) == False
+def alpha_order(lst: List[str]) -> bool:
+    i: int = 0
+    while i < len(lst)-1:
+        if lst[i] > lst[i+1]:
+            return False
+        i += 1
+    return True
+
+print(alpha_order(['cat', 'dog', 'horse', 'platypus']))
+print(alpha_order(['cat', 'dog', 'zebra', 'horse', 'platypus']))
