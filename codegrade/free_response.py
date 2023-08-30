5f4ff9f3aca0a725c2283f44e891591d820a68ea
Brent Yorgey <byorgey@gmail.com>
2023-08-30 05:49:17 -0500

    update free response script

--- codegrade/free_response.py
+++ codegrade/free_response.py
@@ -6,15 +6,17 @@ parser = argparse.ArgumentParser(description='Check a free-response question')
 
 parser.add_argument('md_file', type=str)
 parser.add_argument('--words', type=int)
+parser.add_argument('--blank', type=str)
 parser.set_defaults(words=50)
+parser.set_defaults(blank="Replace")
 
 args = parser.parse_args()
 
 word_count = 0
 with open(args.md_file) as f:
     for line in f.readlines():
-        if '*Replace' in line:
-            print("Answer seems to be blank")
+        if args.blank in line:
+            print("Answer seems to be incomplete")
             sys.exit(1)
         elif 'CodeGrade Tag' not in line:
             word_count += len(line.split())
