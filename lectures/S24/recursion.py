90a4663f5587b5f96efb56ce768ef2cf526ccc9f
gjf2a <ferrer@hendrix.edu>
2024-04-17 08:44:43 -0500

    More recursion

--- lectures/S24/recursion.py
+++ lectures/S24/recursion.py
@@ -37,3 +37,58 @@ def recursive_file_count(filename: str) -> int:
         return count
     else:
         return 1
+
+
+def factorial(n: int) -> int:
+    if n < 0:
+        print("You can't do that!")
+    elif n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+# Recursively define addition, given that our only
+# operator is to add 1 or subtract 1.
+def add(a: int, b: int) -> int:
+    if b == 0:
+        return a
+    else:
+        return 1 + add(a, b - 1)
+
+
+# Recursively define multiplication, given addition
+def times(a: int, b: int) -> int:
+    if b == 0:
+        return 0
+    elif b == 1:
+        return a
+    else:
+        return a + times(a, b - 1)
+
+
+def count_letter(s: str, letter: str) -> int:
+    if s == '':
+        return 0
+    elif s[0] == letter:
+        return 1 + count_letter(s[1:], letter)
+    else:
+        return count_letter(s[1:], letter)
+
+
+def retain_if(s: str, letters_to_keep: str) -> str:
+    if s == '':
+        return ''
+    elif s[0] in letters_to_keep:
+        return s[0] + retain_if(s[1:], letters_to_keep)
+    else:
+        return retain_if(s[1:], letters_to_keep)
+
+
+def has_double_letter(s: str) -> bool:
+    if len(s) < 2:
+        return False
+    elif s[0] == s[1]:
+        return True
+    else:
+        return has_double_letter(s[1:])
