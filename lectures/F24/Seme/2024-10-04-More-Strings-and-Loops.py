18b9899e499750bdd27a20222e8e05d81740228d
larskseme <42153008+larskseme@users.noreply.github.com>
2024-10-04 07:27:36 -0500

    Add files via upload

---
+++ lectures/F24/Seme/2024-10-04-More-Strings-and-Loops.py
@@ -0,0 +1,23 @@
+# Homework -- Function Stack Tracing due Monday
+
+def str_aaa(s: str) -> bool: # return True if s contains at least 3 a's
+    1
+
+
+###########################
+# Input Checking
+
+#Yes/No Response
+
+# returns True if they say yes and False for no
+# can deal with YES, yes, y, Y  vs NO, no n, N
+# and otherwise reasks
+def yes_or_no() -> bool:
+    1
+
+
+# checks for integer input, including things like -2
+def int_input() -> int:
+    1
+
+# float -- should be able to deal with 5, 5., 5.0, -5, -5., -5.0
