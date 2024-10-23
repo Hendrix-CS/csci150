042d37b4dc3c1830062b90e92baa71a56812f922
larskseme <42153008+larskseme@users.noreply.github.com>
2024-10-23 10:44:00 -0500

    Add files via upload

---
+++ lectures/F24/Seme/2024-10-23-More-File-IO.py
@@ -0,0 +1,21 @@
+from typing import *
+
+# Project #2 due one week from today
+# Homework 9 - File IO due on Monday
+#   -- both are turned in on Teams
+
+# Example -- Prompt the user to enter strings
+# continue to prompt until they type 'Exit.'
+# Then ask them for a file name
+# Write out their strings (except 'Exit') one line at a time to that file:
+# #
+def make_file():
+    1
+
+# Read Data File
+# The file 'data-practice.txt' is a comma-delimited
+# file. It is (made-up) data representing customer data:
+# order number, customer ID, purchase price, state
+
+# Write a function which reads in the file and
+#  returns the average purchase for an order
