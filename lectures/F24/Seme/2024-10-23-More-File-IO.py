f1b4dc746b96e95ab6cb3b7595571bf6ed6f093c
larskseme <42153008+larskseme@users.noreply.github.com>
2024-10-23 11:49:56 -0500

    Add files via upload

--- lectures/F24/Seme/2024-10-23-More-File-IO.py
+++ lectures/F24/Seme/2024-10-23-More-File-IO.py
@@ -10,7 +10,34 @@ from typing import *
 # Write out their strings (except 'Exit') one line at a time to that file:
 # #
 def make_file():
-    1
+    f_out = open('output_file.txt','w')
+    cont = True
+
+    while cont:
+        word = input("Enter a word. Use 'Exit' to stop.")
+        if word == 'Exit':
+            cont = False
+        else:
+            f_out.write(word + '\n')
+
+    f_out.close()
+
+def read_data():
+    f_in = open('data-practice.txt','r')
+    header = True
+    sum = 0
+    count = 0
+    for line in f_in.readlines():
+        data = line.strip()
+        data = data.split(',')
+        if not header:
+            count += 1
+            sum += float(data[2][1:])
+
+        header = False
+
+    f_in.close()
+    return sum / count
 
 # Read Data File
 # The file 'data-practice.txt' is a comma-delimited
