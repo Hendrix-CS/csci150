caf04b028e7e28c7ec14e88dc927489495de29a5
larskseme <42153008+larskseme@users.noreply.github.com>
2024-10-29 08:49:43 -0500

    Add files via upload

---
+++ lectures/F24/Seme/2024-10-30-More-Dictionaries.py
@@ -0,0 +1,75 @@
+from typing import *
+
+
+# Reminders: Dictionary Homework due Monday
+#
+
+
+
+# Dictionary Examples --
+
+# Given a list of distinct strings, each string is the key and the value
+# is the number of 't's in the string
+def t_count(lst: List[str]) -> Dict[str,int]:
+    1
+
+# for each integer 0, 1, ..., n:
+# the key is the integer and the value is the
+# number of times you need to //2 to get 0
+
+def div_count(n: int) -> Dict[int,int]:
+    1
+
+# Same idea a before, except the keys are the
+# number of times needed and the value is the
+# list of numbers:
+def rev_div_count(n: int) -> Dict[int,List[int]]:
+    1
+
+# The user will enter  words
+# Each word will do into a dictionary, keyed on word length, with value the list
+#   of words of that length
+#
+# We will exit when the user enters the word 'Exit'
+
+def word_count() -> Dict[int, List[str]]:
+    out_dict = {}  # we need to build a new, empty dictionary
+
+    cont = True  # a flag; will switch to False when the user enters 'Exit'
+
+    while cont:
+        ans = input('Please enter a word ("Exit" to stop): ')
+        if ans == 'Exit':
+            cont = False
+        else:
+            leng = len(ans) # not required, but easier conceptually for me
+
+            # we want to put this information into the dictionary
+            # -- but we do not know if <leng> is currently a valid key!
+            # -- this is a common issue when building a dictionary
+            if leng not in out_dict:
+                out_dict[leng] = []
+
+            out_dict[leng].append(ans)
+
+
+    return out_dict
+#
+#
+# value_find():
+# Suppose you have a dictionary d, which has keys which are strings and
+# values a list of integers
+#
+# Given a particular integer n, we want to return a list of the keys
+# for which n belong to their value:
+#
+# For example:
+# d = {'a': [5, 1, 2, 3], 'b': [1, 2, 3, 4], 'c': [2, 4, 2], 'd': [1, 3, 5]}
+# then value_find(d, 3) would return ['a', 'b', 'd'] and
+# value_find(d, 2) would return ['a', 'b', 'c'] and
+# value_find(d, 7) would return []
+
+def value_find(d: Dict[str, List[int]], n: int) -> List[str]:
+    1
+
+
---
+++ lectures/F24/Seme/2024-11-01-Project3-Discussion.py
@@ -0,0 +1,25 @@
+# Project #3 is now assigned
+#
+# See the class webpage for full details
+#
+# You can do almost anything: Commons options include:
+# -- simple video game
+# -- a very updated version of your Word game -- need to be substantial
+# -- extend an idea from lab -- again substantially
+# -- data analysis
+
+# In class, I will show a few examples of final projects
+#
+# NOTE: You can work alone, or with a partner
+# -- that partner can be in the other section
+
+# Important Dates:
+
+# Friday, November 15 (2 weeks) -- Design Document Due
+#  -- there is a link to a sample on Webpage
+#  -- I strongly encourage you to stop by and talk with me about your idea!
+
+# Friday, December 13, 8:30am - 11:30am -- Project Presentations
+# -- this gives you 6 total weeks to work on the project
+# -- a good project typically takes 15 - 20 hours of work
+# -- please note that late work *will not* be accepted!
