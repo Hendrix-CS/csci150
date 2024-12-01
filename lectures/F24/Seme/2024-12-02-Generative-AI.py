c9178a1fb47034ed54ff17385152b0886cc39518
larskseme <42153008+larskseme@users.noreply.github.com>
2024-12-01 13:36:58 -0600

    Add files via upload

---
+++ lectures/F24/Seme/2024-12-02-Generative-AI.py
@@ -0,0 +1,20 @@
+# Wednesday & Friday in class are primarily about the final project
+# Come with your questions about your project:
+#  - I will wander around and help
+#  - This goes best if you have specific, detailed questions
+#  - It is not a great time for me to help you write a large function/class
+
+# Lab this week is also devoted to the final project
+# - Attendance counts!
+# - There is still an exit form to fill out!
+
+# Grade questions? See me (or Lab instructor if it is primarily about lab)
+
+# In class today:
+#  - We played around with my simple 'Generative AI'
+#  - Code is shared on Teams, since it is multiple files
+#  - How do LLMs (Large Language Models) work?
+
+#  - What is an acceptable use?
+#      - Examples
+#      - Stack Exchange, also
