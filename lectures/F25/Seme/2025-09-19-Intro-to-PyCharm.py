cab7b897b4475c6a984d3e2224f175e3e2f0a1de
larskseme <42153008+larskseme@users.noreply.github.com>
2025-09-19 12:06:54 -0500

    seme 9/19

---
+++ lectures/F25/Seme/2025-09-19-Intro-to-PyCharm.py
@@ -0,0 +1,35 @@
+# This is a .py file -- a single file that the Python interpreter can run
+#
+# I am using the IDE PyCharm to edit and run the file
+#
+# You should download Python3 (python.org) &
+# PyCharm (jetbrains.com/pycharm) -- make sure to download the free "Community Edition"
+
+# Install both
+
+# I will share this file on the class Team page
+# *Download it*
+
+# If you haven't yet, create a folder somewhere for this class
+# Open PyCharm and select 'New Project' from the top menu
+
+# Give the Project a name -- something like 'Code from Class'
+# Navigate to your CSCI 150 folder
+# Make sure the Interpreter type is 'Project Venv'
+# Python Version should be Python3
+
+# I *strongly* recommend you create a new project for 'Labs' another for 'Homework,' etc
+# Some students make new projects for *each* lab instead, which is fine.
+
+
+# Ok, now you are ready to check if your set-up is correct.
+
+# Move this file from your downloads into the folder for this project
+# open it
+# Go to the 'Run' top menu and Run the file
+# If it opens a window at the bottom and prints 'Hello World'
+# and says "Process finished with exit code 0'
+# You have successfully installed Python &  PyCharm. -- You can leave
+
+
+print('Hello World')
