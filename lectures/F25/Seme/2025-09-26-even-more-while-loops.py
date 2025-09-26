8fee3cfd51e5bfaa602c11e0c75ad45f3a973f04
larskseme <42153008+larskseme@users.noreply.github.com>
2025-09-26 09:52:31 -0500

    seme code 9/26

--- lectures/F25/Seme/2025-09-26-even-more-while-loops.py
+++ lectures/F25/Seme/2025-09-26-even-more-while-loops.py
@@ -8,6 +8,19 @@
 # It is an open question in mathematics, called the Collatz Conjecture,
 # whether each starting value of n eventually reaches 1
 #
+
+
+
+
+
+
+
+
+
+
+
+
+
 # According to a recent paper, all numbers up to 2^71 (about 2.36 x 10^21)
 # do eventually reach 1
 # [Barina, David. "Improved verification limit for the convergence of the Collatz conjecture,"
@@ -26,3 +39,67 @@
 # Ask the user for input value
 
 # Have a main() which controls everything
+
+def print_rules():
+    print('This program will ask you for a starting number')
+    print('which should be a positive integer.')
+    print()
+    print('It will then test all values from 1 to that number')
+    print('and return the number of steps the Hailstone Function')
+    print('needs to reach 1.')
+    print()
+
+def get_ending_number():
+    success = False
+    while not success:
+        ans = input('Please enter a positive integer: ')
+        if ans.isdigit() and int(ans) > 0:
+            n = int(ans)
+            success = True
+        else:
+            print('I did not understand. Please try again.')
+    return n
+
+# for check_up_to_n() we need:
+# to write Hailstone
+# keep track of steps
+# print out answer
+# go to the next number
+
+def hailstone(n: int) -> int:
+    if n % 2 == 0:
+        return n // 2
+    else:
+        return 3 * n + 1
+
+def step_count(n: int) -> int:
+    steps = 0
+    while n != 1:
+        n = hailstone(n)
+        steps += 1
+    return steps
+
+def check_up_to_n(n: int):
+    start = 1
+    while start <= n:
+        steps = step_count(start)
+        print(f'The number {start} takes {steps} steps to reach 1.')
+        start += 1
+
+
+
+
+
+
+def main():
+    print_rules()
+    n = get_ending_number()
+    check_up_to_n(n)
+
+main()
+
+
+
+
+
+
