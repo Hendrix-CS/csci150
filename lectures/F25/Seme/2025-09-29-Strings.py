a60039edc5a073983ce5b1c93276443c6e1941e9
larskseme <42153008+larskseme@users.noreply.github.com>
2025-09-29 10:01:59 -0500

    seme 9/29

--- lectures/F25/Seme/2025-09-29-Strings.py
+++ lectures/F25/Seme/2025-09-29-Strings.py
@@ -22,8 +22,10 @@ t = 'abcdefg'
 
 # print(s + t)
 # print(t + s)
-#
+# #
 # print(s * 3)
+#
+# print(s * -10)
 
 
 # We can access the individual characters of a string by their index.
@@ -33,6 +35,14 @@ t = 'abcdefg'
 # print(s[0])
 # print(s[5])
 # print(t[3])
+#
+# print(t[6])
+
+# negative indices
+
+# print(s[-1])
+# print(s[-3])
+# print(s[-10])
 
 
 # We can also get a 'slice' of a string:
@@ -40,19 +50,21 @@ t = 'abcdefg'
 # print(s[1:6])
 # print(s[1:2])
 # print(s[1:3])
+#
+# print(t[2:2+3])
 
 # The syntax is str[a:b] will return from index a to index b - 1.
 # This is also weird, but again, you get used to it
 
 # print(s[4:])  #starts at index 4, goes to end
-#
+# #
 # print(s[:4])  # starts at index goes, goes up to, does not include 4
-#
+# #
 # print(s[:4] + s[4:])
 
-#print(s[:])  # starts at 0, goes to end (i.e. the whole string)
+# print(s[:])  # starts at 0, goes to end (i.e. the whole string)
 
-#print(s[4:4]) # prints ''
+# print(s[4:4]) # prints ''
 
 # The empty string ''
 
@@ -101,37 +113,37 @@ t = 'abcdefg'
 # # Length -- a function, which returns the number of characters in a string
 # print(len(s))
 # print(len('aaa'))
-# # #
+# # # #
 # print(len(''))
-#
+# #
 # print(s[0: len(s)])
 #
-# print(len(s) == len(t))
+#print(len(s) == len(t))
 
 
 # Other examples (these are all methods)
 
-# upper:
+# # upper/lower:
 # print(s.lower())
+# print(s.upper())
 # print(s)
-
+#
 # s = s.upper()
-# #print(s.upper())
+#
 # print(s)
-#
-# # lower
-#print(s.lower())
-#
-# print(s) #notice that s.lower() and s.upper() don't actually change s
-#
+# #
+# # # lower
+# #print(s.lower())
+# #
+# # print(s) #notice that s.lower() and s.upper() don't actually change s
+# #
 # print('fbXtR%& *'.lower())
 
 # count
-
-# print(s.count('x'))
+#print(s.count('x'))
 # print(s.count('n'))
 # #
-# print('abcABCabcdeab'.count('abc'))
+#print('abcABCabcdeab'.count('abc'))
 
 #print('abcABCabcdeab'.count('a'))
 
@@ -139,18 +151,19 @@ t = 'abcdefg'
 
 #print(s.find('t'))
 #
-#print('abcdabcdabcde'.find('z'))
+#print('abcdabcdabcde'.find('d'))
 
 # replace
-
+#
 # print(s.replace('y','TTT'))
-# #
+# # #
 # print('abcdabcdabcd'.replace('b',"X"))
+# print(s)
 
 
-#print('12303'.isdigit())
+print('12303'.isdigit())
 
-#print('-12303'.isdigit())
+print('-12303'.isdigit())
 
 # If time allows, we'll consider these two examples below:
 
