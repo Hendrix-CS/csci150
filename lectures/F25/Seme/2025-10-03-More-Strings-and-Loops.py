14c14931beb0bdd542375f4f513c9ae2b7ce8830
larskseme <42153008+larskseme@users.noreply.github.com>
2025-10-03 09:48:46 -0500

    update seme 10/3

--- lectures/F25/Seme/2025-10-03-More-Strings-and-Loops.py
+++ lectures/F25/Seme/2025-10-03-More-Strings-and-Loops.py
@@ -1,7 +1,54 @@
 # Homework #5 -- Strings due Monday
 
+
+####
+# Leftover from Wednesday
+def is_inorder(s: str) -> bool:
+
+    i = 0
+    while i < len(s) - 1:
+        if s[i] > s[i + 1]:
+
+            return False
+
+        i += 1
+
+    return True
+
+
+# Note the len(s) - 1 above
+# When comparing, if the string has 5 characters, we only need to do 4 comparisons
+# forgetting the -1 will lead to an index error, which you then fix
+
+
+
 def str_aaa(s: str) -> bool: # return True if s contains at least 3 a's
-    1
+
+    a_count = 0
+    i = 0
+    while i < len(s):
+        if s[i] == 'a':
+            a_count += 1
+
+        i += 1
+
+    if a_count < 3:
+        return False
+    else:
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -14,6 +61,32 @@ def str_aaa(s: str) -> bool: # return True if s contains at least 3 a's
 # can deal with YES, yes, y, Y  vs NO, no n, N
 # and otherwise reasks
 def yes_or_no() -> bool:
+
+    success = False
+    while not success:
+        ans = input('Please answer yes or no: ')
+        ans = ans.lower()
+        if ans == 'yes' or ans == 'y':
+            return True
+        elif ans == 'no' or ans == 'n':
+            return False
+        else:
+            print('I did not understand. Please try again.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     success = False
 
     while not success:
@@ -28,17 +101,21 @@ def yes_or_no() -> bool:
             print('Please just enter yes or no. ')
 
 
+# PLEASE -- looks at the below code before class on Monday.  We'll talk about it then.
+
+
 # checks for integer input, including things like -2
 def int_input() -> int:
-    success = False
-    while not success:
-        ans = input('Please enter an integer: ')
-        if ans.isdigit():
-            return int(ans)
-        elif len(ans) > 0 and ans[0] == '-' and ans[1:].isdigit():
-            return int(ans)
-        else:
-            print('Please try again')
+    1
+    # success = False
+    # while not success:
+    #     ans = input('Please enter an integer: ')
+    #     if ans.isdigit():
+    #         return int(ans)
+    #     elif len(ans) > 0 and ans[0] == '-' and ans[1:].isdigit():
+    #         return int(ans)
+    #     else:
+    #         print('Please try again')
 
 
 
