3f7eaceaba3ffb2f1dc1e5f565df8db826f584f6
larskseme <42153008+larskseme@users.noreply.github.com>
2025-10-06 11:08:18 -0500

    update seme 10/6

--- lectures/F25/Seme/2025-10-06-Lists.py
+++ lectures/F25/Seme/2025-10-06-Lists.py
@@ -7,7 +7,7 @@ from typing import *
 
 # we can make other ordered collections:
 
-# my_list: List[int] = [7, 3, 4, 7, 2]
+#my_list: List[int] = [7, 3, 4, 7, 2]
 #
 ## # Type declarations: Though not required above, this is how we let a
 # # human reader (and Pycharm) know
@@ -16,9 +16,9 @@ from typing import *
 #
 # # Compare:
 #
-# 'dog->dot->lot->let'
+#'dog->dot->lot->let'
 #
-# sol_list: List[str] = ['dog','dot','lot','let']
+#sol_list: List[str] = ['dog','dot','lot','let']
 #
 #
 
@@ -31,7 +31,7 @@ from typing import *
 #
 # # the individual entries in a list can be anything:
 #
-# other_list = [4, 6, 'a', '6', True, [6,'abc'],3.14159]
+#other_list = [4, 6, 'a', '6', True, [6,'abc'],3.14159]
 #
 # # but you really shouldn't do this. Python itself does not care,
 # # but you will make mistakes -- this is hard for a human to conceptualize
@@ -42,20 +42,25 @@ from typing import *
 # # Basic list construction/manipulation:
 #
 # # the empty list:
-# lst = []
+#lst = []
+
+
+
 #
 # # indices -- work just like strings:
 #
-# my_list = [7, 3, 4, 7, 2]
+my_list = [7, 3, 4, 7, 2]
 #
 # print(my_list[1])
+# print(my_list[1:4])
 # print(my_list[1:2])
+# print(my_list[1:1])
 # #
 # # print(my_list[1:4])
 #
 # #the 'in' operation:
 # print(4 in my_list)
-# #
+# # #
 # print(10 in my_list)
 #
 # # (in works for strings as well --)
@@ -64,12 +69,12 @@ from typing import *
 #
 # print('s' in 'my_string')
 # print('x' in 'my_string')
-# print('rin' in 'my_string')
+# print('rn' in 'my_string')
 
-# print([3] in [[3],5,7,8])
+# print(3 in [[3],5,7,8])
 # print([3,5] in [4, 7,[3,5],7,8])
 #
-# #print([1,2,3] == [1,3,2])
+#print([1,2,3] == [1,3,2])
 #
 # # In many ways, lists and strings are similar
 # # There is a fundamental difference, though:
@@ -78,71 +83,97 @@ from typing import *
 # # and cannot be changed.
 #
 # # Lists are mutable. You can change individual entries:
-# my_list = [7, 2, 3, 5, 7]
-# print(my_list)
-# #
+#my_list = [7, 2, 3, 5, 7]
+#print(my_list)
+#
 # my_list[1] = 1234
 # #
 # print(my_list)
-# # #
+# # # #
 # s = 'my_string'
 # print(s)
-# #s[4] = 'g'
+# s[4] = 'g'
 # print(s)
 #
+
+x = 5
+y = x
+x = x + 1
+# print(x)
+# print(y)
+
+
+
 
 
 # Complication -- we will talk about this later this semester
 # a = [1, 2, 3]
 # b = a
-# b[1] = 5
+# a[1] = 5
 # print(a)
 # print(b)
 
 ## Operations on lists
 # We can add elements to a list with append()
-# my_list = [7, 2, 3, 5, 7]
-# my_list.append(888)
+my_list = [7, 2, 3, 5, 7]
+# my_list.append(1234)
 # print(my_list)
-
-# Append always adds to the end.
-
-# You can add two lists:
-
+#
+# # Append always adds to the end.
+#
+# # You can add two lists:
+#
 # print([1,2,3] + [7,5,3])
-# #
-# # # You can multiply a list by a non-negative integer:
+# # #
+# # # # You can multiply a list by a non-negative integer:
 # print([1,2,3] * 4)
 # #
 # # # #Length
 # print(my_list)
 # print(len(my_list))
-#
-#print(len([[3, 5], 7, 8]))
+# #
+# print(len([[3, 5], 7, 8]))
 
 
 # List operations:
-# pop() -- removes from the list by index *and* returns that value
-# my_list = [7,2,4,8,1]
+#pop() -- removes from the list by index *and* returns that value
+my_list = [7,2,7,8,1]
 # print(my_list)
-# x = my_list.pop(3)
+#x = my_list.pop(10)
 # print(my_list)
 # print(x)
 #
 # # remove() - removes by *value* and does not return anything -- only removes the first occurrence
-# my_list.remove(7)
+# my_list.remove(6)
 # print(my_list)
 
 #split() - given a string and a character (or sequence of characters), returns a list, split on the character.
 # string1 = 'hi there how are you today?'
 # print(string1.split(' '))
-# print(string1.split(' h'))
-# print(string1.split('h'))
+# #print(string1.split(' h'))
+# print(string1.split('z'))
 
 # join() -- the opposite of split:  str.join(list) will return a string with a the value of str between each entry
 # only works if the original list is made up of string entries
 
-# print(' ** %trh '.join(['Hi','there','how','are','you?']))
+print(';'.join(['Hi','there','how','are','you?']))
 #
-# lst = ['dog','log','lot','tot']
-# print('->'.join(lst))
+lst = ['dog','log','lot','tot']
+print('->'.join(lst))
+
+my_list = [4, 6, 7, 10, 1]
+
+def sum_list(lst: List[int]) -> int:
+    s = 0
+    i = 0
+    while i < len(lst):
+        s += lst[i]
+
+        i += 1
+
+    return s
+
+print(my_list)
+print(sum_list(my_list))
+
+
