29048bb797bb4c4bc86088c4bc209526505d68e7
Brent Yorgey <byorgey@gmail.com>
2025-10-10 11:51:06 -0500

    yorgey 10/10

--- lectures/F25/Yorgey/hilow.py
+++ lectures/F25/Yorgey/hilow.py
@@ -1,11 +1,21 @@
 import random
 
-repeat = "yes"
-while repeat == "yes":
+def print_instructions():
     print("This is the game of HIGH and LOW. The computer will pick a")
     print("random number, and you will need to guess if the next number")
     print("picked by the computer will be higher than the current")
     print("number. You get 10 chances to guess.")
+
+def process_guess(guess: str, r: int, next_r: int) -> bool:
+    if (guess == "yes" and next_r > r) or (guess == "no" and next_r <= r):
+        print("You were right!")
+        return True
+    else:
+        print("Sorry, you were wrong.")
+        return False
+
+# Play the game once and return the number of correct guesses.
+def play_game() -> int:
     count = 0
     correct = 0
     r = random.randint(1, 10)
@@ -14,13 +24,20 @@ while repeat == "yes":
         guess = input("Do you think the next number will be higher (yes or no)? ")
         next_r = random.randint(1, 10)
         print(f"The next number is {next_r}.")
-        if (guess == "yes" and next_r > r) or (guess == "no" and next_r <= r):
+        if process_guess(guess, r, next_r):
             correct += 1
-            print("You were right!")
-        else:
-            print("Sorry, you were wrong.")
         r = next_r
         count += 1
-    print(f"You got {correct} out of {count} guesses correct!")
-    repeat = input("Do you want to play again? ")
-print("Thanks for playing!")
+
+    return correct
+
+def main():
+    repeat = "yes"
+    while repeat == "yes":
+        print_instructions()
+        correct = play_game()
+        print(f"You got {correct} out of 10 guesses correct!")
+        repeat = input("Do you want to play again? ")
+    print("Thanks for playing!")
+
+main()
