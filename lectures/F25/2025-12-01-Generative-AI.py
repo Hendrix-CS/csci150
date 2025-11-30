1085069dadb8b657cf587e53edb6da39a2d46e90
larskseme <42153008+larskseme@users.noreply.github.com>
2025-11-30 16:33:49 -0600

    seme 12/1

---
+++ lectures/F25/2025-12-01-Generative-AI.py
@@ -0,0 +1,73 @@
+# Wednesday (and some of Friday?) in class are primarily about the final project
+# Come with your questions about your project:
+#  - I will wander around and help
+#  - This goes best if you have specific, detailed questions
+#  - It is not a great time for me to help you write a large function/class
+
+# Lab this week is also devoted to the final project
+# - Attendance counts!
+
+# Grade questions? See me (or Lab instructor if it is primarily about lab)
+
+
+# "Thinking" Machines
+
+# Eliza -- the first chatbot -- implementation at https://web.njit.edu/~ronkowit/eliza.html
+
+# Machine Learning -- various type of trial-and-error learning
+# - optimization problems
+# - genetic algorithms
+# - neural networks
+
+# Generative AI & LLMs
+# - use a (large) corpus of text
+# - make predictions of the next word(s) based on previous word(s)
+
+
+# Ethical Issues
+# - where does the text come from -- copyright?
+# - resource usage
+# - what are the incentives?
+
+# They choose the "mostly likely" next word/phrase and sound reasonable
+# “I’ll go down this thread with [Chat]GPT or Grok and I’ll start to get
+# to the edge of what’s known in quantum physics and then I’m doing the equivalent of vibe coding,
+# except it’s vibe physics,” Kalanick explained. “And we’re approaching what’s known.
+# And I’m trying to poke and see if there’s breakthroughs to be had.
+# And I’ve gotten pretty damn close to some interesting breakthroughs just doing that.”
+
+# How would he (a non-physicist) know he was "close"
+# My personal experience -- in domains I know nothing or a little bit about
+# the answers often seem reasonable (and some of them are probably correct)
+# but in domains I have expertise in, I regularly spot subtle (and sometimes not subtle) errors!
+
+# The "Hallucination" problem
+# - made up citations
+# - claims that are obviously false
+# - claims certainty
+
+# Academic World
+# - what is the point of assignments?
+# - incentives can be misaligned
+# - if a computer writes the paper and a computer grades the paper ...
+
+
+# Fun!
+# - Shakespeare sonnet generator
+# - he wrote 154 sonnets
+# - each(*) are 14 lines, 3 quatrains in iambic pentameter, ABAB
+#    followed by a rhyming couplet.
+
+
+# Other text generator
+
+
+
+# In class today:
+#  - We played around with my simple 'Generative AI'
+#  - Code is shared on Teams, since it is multiple files
+#  - How do LLMs (Large Language Models) work?
+
+#  - What is an acceptable use?
+#      - Examples
+#      - Stack Exchange, also
