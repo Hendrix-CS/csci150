15cedbaba7c961e359d30633727afe1710e01e7d
larskseme <42153008+larskseme@users.noreply.github.com>
2026-02-19 09:48:12 -0600

    code 2/20

---
+++ lectures/S26/2026-02-20-Even-More-While-Loops.py
@@ -0,0 +1,75 @@
+# New Homework -- Loops on CodingBat
+
+# As noted in the homework itself, you need to make a (free) account on CodingBat
+# codingbat.com/python
+
+# Project #1 is due One Week from Today. Please turn in to the Assignment on Teams
+
+# You also received a grade update from me by email
+# -- if you have any questions or something is missing/incorrect
+#    please let me know!
+
+###### We did not get to the code below here.  I'll start with the ideas below on Friday
+
+
+
+# def entry() -> int:
+#     successful = False
+#     while not successful:
+#         str_n = input('Enter an integer between 2 and 5, inclusive: ')
+#         if str_n.isdigit(): # new command .isdigit()
+#             n = int(str_n)
+#             if 2 <= n <= 5:
+#                 successful = True
+#         else:
+#             print('Please follow directions!')
+#
+#     return n
+
+
+
+# This uses a 'sentinel' flag
+# Basic form
+
+# sentinel = False
+# while not sentinel:
+#      query the user for input
+#      if input is of the correct form:
+#           sentinel = True
+#      else:
+#           print('That is not a valid answer. Please try again.')
+
+
+# Continually prompt the user to enter 'y' or 'n' -- when they do, return that value
+
+def y_or_n() -> str:
+    1
+
+
+
+#############################
+# We will write a large piece of code:
+# Prompt the user to enter a positive integer (say n)
+# For each 1, 2, 3, ..., n the code will
+# print out the number of steps it takes to reach 1 under the Hailstone Function
+
+# We have a lot of the pieces already written:
+
+def hailstone(n: int) -> int:
+    if n % 2 == 0:
+        return n // 2
+    else:
+        return 3 * n + 1
+
+def collatz_steps(n: int) -> int:
+    steps = 0
+    while n != 1:
+        n = hailstone(n)
+        steps += 1
+    return steps
+
+## New function:
+#
+#main()
+# needs to ask for an integer >= 1
+# for each i between 1 and n, run collatz_steps and print answer
