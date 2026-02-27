b66748f5f07cb1f586f3f2224f9a6b5bfa482085
larskseme <42153008+larskseme@users.noreply.github.com>
2026-02-26 19:34:45 -0600

    Add files via upload

---
+++ lectures/S26/2026-02-27-File-Input-Output.py
@@ -0,0 +1,57 @@
+import random
+import spellcheck
+
+# Quiz #4 Today -- While Loops
+# Homework #5 Strings, due Monday
+#
+# I have completed assesing all projects which have been turned in.
+# Please check if you warned all points and make sure you understand
+#    what to do if you did not.
+
+# Project #2 is assigned today
+
+# Let over from Wednesday:
+
+# Finally, write in_order(s), which takes in a string and returns
+# True when the letters are in alphabetical order -- counting repeats as being ok
+
+# 'loop' would return True
+# 'while' would return False
+
+def in_order(s: str) -> bool:
+    1
+
+
+
+## Reading from and writing to a file
+
+# Reading from a file
+
+# There are multiple ways to read in a file.  From the 'spellcheck.py' module:
+
+# f = open(wordfile, "r")
+# for line in f:
+#     data = line.strip()
+#
+# f.close()
+
+# data will then loop through each line of the file named <wordfile>
+
+# Or you cna read the whole file at once:
+# f = open(wordfile, "r")
+# t = f.readlines()
+#
+# f.close()
+
+# t would be a list of word -- we'll talk about lists formally next week
+
+# Writing to a file
+# f = open('temp.txt','w')  # the 'w' means you can write!
+# f.write('testing')
+# f.close()
+
+# But *be careful*.  If you write to a file, Python will write over *ANYTHING* that is already there!
+
+# When you read in, the incoming information is *always* a string
+
+# When you write, the outgoing info must be a string!
