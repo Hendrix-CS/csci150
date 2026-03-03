674c7d1585554a92056b03ae14e2e4169096a043
larskseme <42153008+larskseme@users.noreply.github.com>
2026-03-03 14:21:59 -0600

    code for 3/4 and 3/6

---
+++ lectures/S26/2026-03-04-List-Processing.py
@@ -0,0 +1,54 @@
+# Reminders
+# Quiz #5 (Strings) this Friday
+#
+# New Homework #6 (Lists) Assigned today -- due Monday
+#
+# Project #2 due in about 2 weeks
+
+# Last time, we saw that lists are a lot like strings:
+# * Ordered collection of items
+# * Have indices
+# * Have length
+# * Can access individual items or slices
+
+# The basics of processing a list *also* works a lot like strings:
+#
+#
+
+# Given a list of integers, return the sum:
+def list_sum(lst: list[int]) -> int:
+    1
+
+# even_sum -- given a list of integers, return the sum,
+# but only of the *even* numbers in the list
+def even_sum(lst: list[int]) -> int:
+    1
+
+
+# Given a list of strings, return True only if every word is at least 6 letters long
+def long_words(lst: list[str]) -> bool:
+    1
+
+# valid_words
+# Given a list of strings, return True only if all are valid, using:
+# * the spellcheck.py module &
+# * the english3.txt dictionary
+# from the Doublets lab
+#
+# You will need to make sure that both above files are in the same
+# folder as this file
+
+def valid_words(lst: list[str]) -> bool:
+    1
+
+
+# Given a list of integers, triple any value that is even
+def triple_evens(lst: list[int]):
+    1
+
+
+# Given a list of integers, return a *new* list
+# which triples any value that is even
+def triple_evens_new(lst: list[int]) -> list[int]:
+    1
+    
