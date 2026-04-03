7f56ce46ddf5db47266663081d76b474cdde0b29
larskseme <42153008+larskseme@users.noreply.github.com>
2026-04-03 12:07:44 -0500

    update code 4/3

--- lectures/S26/2026-04-03-Dictionaries-and-Data.py
+++ lectures/S26/2026-04-03-Dictionaries-and-Data.py
@@ -13,3 +13,53 @@
 # We will work to understand the data set and
 # build a dictionary (or maybe series of dictionaries)
 # to help us answer some problems
+
+
+
+# We will create a dictionary, keyed on State abbreviation:
+#   * value is another dictionary, keyed on City
+#       * value is zipcode
+
+
+def state_city_dict():
+
+    out_dict = {}
+
+    f = open('zipcodes.txt','r')
+    for line in f.readlines():
+        data = line.split(';')
+        state = data[1]
+        city = data[2]
+        zipc = data[0]
+
+        if state not in out_dict:
+            out_dict[state] = {}
+
+        if city not in out_dict[state]:
+            out_dict[state][city] = []
+
+        out_dict[state][city].append(zipc)
+
+
+
+
+
+
+
+
+    f.close()
+
+    return out_dict
+
+
+
+
+
+
+
+
+
+
+
+
+
