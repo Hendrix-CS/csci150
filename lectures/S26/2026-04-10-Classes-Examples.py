c6c532c8ee94920b6993a3c803b85dc6e16e79e7
larskseme <42153008+larskseme@users.noreply.github.com>
2026-04-09 19:04:27 -0500

    code 4/10

---
+++ lectures/S26/2026-04-10-Classes-Examples.py
@@ -0,0 +1,11 @@
+# Quiz Today -- Dictionaries
+# Homework Due Monday -- Classes
+# Final Project Design Document -- Due Next Friday
+#   * If working with partner, please *both* should turn in a copy
+#   * If you are electing to not to a Final Project
+#          please turn in a short document saying so
+
+
+##########
+# We then ran Dr. Yorgey's RPG game and talked about the Class usages
+# These files are on the webpage
