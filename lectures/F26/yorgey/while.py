30cf60a02a96a703b33c79d61649819389c29612
Brent Yorgey <byorgey@gmail.com>
2026-09-23 10:07:58 -0500

    yorgey 9/23

--- lectures/F26/yorgey/while.py
+++ lectures/F26/yorgey/while.py
@@ -29,11 +29,11 @@ def hellos():
 
 def hellos_loop():
     count = 0
-    while count >= 0:
+    while count < 6:
         print(f'Hello {count}')
         count = count + 1
 
-hellos_loop()
+# hellos_loop()
 
 # Common pattern with while loops:
 #
@@ -41,3 +41,54 @@ hellos_loop()
 # 2. while (something with variable):
 #      *do some stuff*
 #      3. update the variable as the last step in the loop
+
+# add_up_to(n) returns the sum 1 + 2 + 3 + .... + n.
+def add_up_to(n: int) -> int:
+    sum: int = 0    # ': int' optional, tells PyCharm what type of values we intend to store in sum
+
+    count: int = 1
+    while count <= n:
+        sum = sum + count
+
+        count = count + 1
+
+    return sum
+
+# Add up all the odd numbers 1 + 3 + 5 + .... up to n.
+# e.g.  add_odds(5) should be   1 + 3 + 5.
+def add_odds(n: int) -> int:
+    sum: int = 0
+
+    count: int = 1
+    while count <= n:
+        sum = sum + count
+
+        count = count + 2
+
+    return sum
+
+def add_odds2(n: int) -> int:
+    sum: int = 0
+
+    count: int = 1
+    while count <= n:
+        if count % 2 == 1:
+            sum = sum + count
+
+        count = count + 1
+
+    return sum
+
+# Prompt the user to enter numbers, keep adding them up until the user enters 0
+def add_numbers() -> int:
+    sum: int = 0
+
+    done: bool = False   # A boolean variable to control the loop
+    while not done:
+        n = int(input('Please enter a number (0 to stop): '))
+        if n == 0:
+            done = True
+        else:
+            sum = sum + n
+
+    return sum
