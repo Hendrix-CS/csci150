b38e4254affc0d066b4a6ef314bbbf99b1e77974
larskseme <42153008+larskseme@users.noreply.github.com>
2026-09-23 07:09:01 -0500

    seme 9/23

---
+++ lectures/F26/seme/2026-09-23-More-While-Loops.py
@@ -0,0 +1,39 @@
+#### Reminders
+#
+# * Homework #4 Assigned Today (see below)
+# * Quiz Friday - Function Stack
+# * Project #1 Due in one week -- Wednesday, Sept 30
+
+# Homework #4 --
+# In addition to paper & pencil homework, you'll be using CodingBat
+# You can go to https://codingbat.com/python
+# and make a free account -- please use your Hendrix email as your ID
+# password can be whatever you like
+# Under 'pref' menu, put my email (seme@hendrix.edu) in the 'Teacher Share' box!
+
+# Let's do more examples of using a while loop:
+
+def factorial(n) -> int: # returns n! (5! = 5 * 4 * 3 * 2 * 1), for example
+    1
+
+
+def add_evens(n: int) -> int:  # will return the sum of the non-negative even integers < n
+    1
+
+# write function add_up which prompts the user to enter an integer
+# when it keeps a running total, and stops when they enter 0
+
+def add_up() -> int:
+    1
+
+
+# New built-in function: .isdigit()
+# s.isdigit() will return True if the string s contains *only* 0, 1, 2, ..., 9 as characters
+
+def enter_integer() -> int:
+    1
+
+
+# This is an example of a 'sentinel' while loop --
+#    it repeats asking for information (in this case, an integer)
+#    until the user correctly does so
